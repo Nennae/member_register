d91cd4516af88e65ea9503a73604ab1e0092810e
Nennae <sezenerdal@outlook.com>
2022-08-15 18:00:00 +0200

    added image + form

--- app.js
+++ app.js
@@ -1,4 +1,6 @@
+import { MongoClient } from 'mongodb';
 import express from 'express';
+
 const port = 3000;
 const app = express();
 
@@ -6,8 +8,10 @@ app.set('views', './views');
 app.set('view engine', 'ejs');
 
 // Skriv all annan kod här sen
+app.use(express.static('public'));
 
 app.listen(port, () => console.log(`Listening on ${port}`));
+
 
 const client = new MongoClient('mongodb://localhost:27017');
 await client.connect();
@@ -15,36 +19,28 @@ const db = client.db('members');
 const memberList = db.collection('users');
 
 app.get('/users', async (req, res) => {
-      const users = await memberList.find({name}).toArray();
+      const users = await memberList.find({}).toArray();
       res.json(users);
 });
 
+app.get('/members', async (req, res) => {
+      const users = await memberList.find({}).toArray();
+      res.render('members', {users, title: 'Members'});
+});
 
+app.get('/', (req, res) => {
+      res.render('index', {title: 'Home'});
+})
 
-// const express = require('express');
-// const app = express();
-
-// import { MongoClient } from 'mongodb';
-
-// app.set('view engine', 'ejs');
-// app.listen(3000);
-
-// app.get('/', (req, res) => {
-//       res.render('index');
-// })
-
-// app.get('/members', (req, res) => {
-//       res.render('memebers');
-// })
+app.get('/form', (req, res) => {
+      res.render('form', {title: 'Register'});
+})
 
 // app.get('/member', (req, res) => {
 //       res.render('memeber');
 // })
 
-// app.get('/form', (req, res) => {
-//       res.render('form');
-// })
 
-// app.use((req, res) => {
-//       res.status(404).render('404');
-// })
+app.use((req, res) => {
+      res.status(404).render('404');
+})
