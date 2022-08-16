653771efeb5ae2b72586191357e96466a7eebb28
Nennae <sezenerdal@outlook.com>
2022-08-16 17:39:56 +0200

    app functions for G grade done

--- app.js
+++ app.js
@@ -1,4 +1,4 @@
-import { MongoClient } from 'mongodb';
+import { MongoClient, ObjectId } from 'mongodb';
 import express from 'express';
 
 const port = 3000;
@@ -6,41 +6,56 @@ const app = express();
 
 app.set('views', './views');
 app.set('view engine', 'ejs');
+app.use(express.urlencoded({extended: true}));
 
-// Skriv all annan kod här sen
 app.use(express.static('public'));
-
-app.listen(port, () => console.log(`Listening on ${port}`));
-
 
 const client = new MongoClient('mongodb://localhost:27017');
 await client.connect();
-const db = client.db('members');
-const memberList = db.collection('users');
+const db = client.db('secretClub');
+const memberList = db.collection('members');
 
-app.get('/users', async (req, res) => {
-      const users = await memberList.find({}).toArray();
-      res.json(users);
-});
+
+app.get('/', (req, res) => {
+      res.render('index');
+})
 
 app.get('/members', async (req, res) => {
       const users = await memberList.find({}).toArray();
-      res.render('members', {users, title: 'Members'});
+      res.render('members', {users});
 });
 
-app.get('/', (req, res) => {
-      res.render('index', {title: 'Home'});
-})
+app.get('/member/:id', async (req, res) => {
+      const member = await memberList.findOne({ _id: ObjectId(req.params.id) });
+      res.render('member', {
+            name: member.name,
+            mail: member.mail,
+            phone: member.phone,
+            createdAt: member.createdAt,
+            secret: member.secret,
+            id: member._id
+      });
+});
+
+app.get('/member/:id/delete', async (req, res) => {
+      await memberList.deleteOne({ _id: ObjectId(req.params.id) });
+      res.redirect('/members');
+});
 
 app.get('/form', (req, res) => {
-      res.render('form', {title: 'Register'});
+      res.render('form');
 })
 
-// app.get('/member', (req, res) => {
-//       res.render('memeber');
+app.post('/members/form', async (req, res) => {
+      await memberList.insertOne(req.body);
+      res.redirect('/members');
+});
+app.get('/members/form', (req, res) => {
+      res.render('form');
+});
+
+// app.use((req, res) => {
+//       res.status(404).render('404');
 // })
 
-
-app.use((req, res) => {
-      res.status(404).render('404');
-})
+app.listen(port, () => console.log(`Listening on ${port}`));
